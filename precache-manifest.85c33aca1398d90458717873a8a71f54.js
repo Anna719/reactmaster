e36894ddf75621910899d4f6e64eec1473720b3a
Anna Stelmakh <anna.stelmakh@student.tuke.sk>
2021-02-18 19:40:40 +0200

    Updates

---
+++ precache-manifest.85c33aca1398d90458717873a8a71f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667d795eaffa36ddc421f79e71755da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e231cd2b2146871732f8",
+    "url": "/static/css/main.5f96ef7b.chunk.css"
+  },
+  {
+    "revision": "3ebadea35fc5c37b6dda",
+    "url": "/static/js/2.f18d2ef6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f18d2ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e231cd2b2146871732f8",
+    "url": "/static/js/main.4ccaa0e4.chunk.js"
+  },
+  {
+    "revision": "d7bfa71de0ae1917c6ad",
+    "url": "/static/js/runtime-main.028bcb86.js"
+  },
+  {
+    "revision": "987003f7a016ff6b55bb723746d46e03",
+    "url": "/static/media/nature.987003f7.jpg"
+  }
+]);
